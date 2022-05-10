d2dd797e3343796e414a194228371f8ecda44c74
yarafarid <87917403+yarafarid@users.noreply.github.com>
2022-05-10 11:04:11 +0200

    Add files via upload

---
+++ Button.c
@@ -0,0 +1,22 @@
+void Button_vInit(unsigned char portName , unsigned char pinNo);
+unsigned char Button_u8read(unsigned char portName , unsigned char pinNo);
+void Button_vInit_PUll_UP(unsigned char portName , unsigned char pinNo);
+
+
+
+void Button_vInit(unsigned char portName , unsigned char pinNo){
+
+	PORT_Init(portname) ;  // OR INTIALIZE ALL PORTS AS DIGITAL IN MAIN
+	DIO_vSETbit_DIR(portName, pinNo , 0); // DIR = 0 , SINCE BUTTON IS INPUT
+}
+
+unsigned char Button_u8read(unsigned char portName , unsigned char pinNo){
+ return DIO_u8readbit(portName , pinNo) ;
+}
+
+
+void Button_vInit_PUll_UP(unsigned char portName , unsigned char pinNo){
+	DIO_vEnablePullup(portName,  pinNo) ;
+	PORT_Init(portname) ;  // OR INTIALIZE ALL PORTS AS DIGITAL IN MAIN
+	DIO_vSETbit_DIR(portName, pinNo , 0); // DIR = 0 , SINCE BUTTON IS INPUT
+}
