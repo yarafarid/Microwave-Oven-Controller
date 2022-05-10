20e0f15edd2bab5342afda3765f3d873ebe8b51b
ShahdAhmed1900719 <105251955+ShahdAhmed1900719@users.noreply.github.com>
2022-05-10 23:34:12 +0200

    Add files via upload

---
+++ LEDS.h
@@ -0,0 +1,4 @@
+void LED_Int(unsigned char portName,unsigned char pinNO);
+void LED_On(unsigned char portName,unsigned char pinNO);
+void LED_Off(unsigned char portName,unsigned char pinNO);
+void LED_Togl(unsigned char portName,unsigned char pinNO);
