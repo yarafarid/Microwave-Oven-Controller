b980df7a221ce6c04291bd8f1804b87515f639ec
FarahMohamed27 <105306483+FarahMohamed27@users.noreply.github.com>
2022-05-10 23:02:19 +0200

    Delete initialization1.h

--- initialization1.h
+++
@@ -1,9 +0,0 @@
-void GPIO_PinDirection (unsigned char PortName, unsigned char PinNumber, unsigned char Direction);
-	
-void GPIO_PinToggle (unsigned char PortName, unsigned char PinNumber);
-
-unsigned char Port_Read (unsigned char PortName)
-
-void Port_Write (unsigned char PortName, unsigned char Data);
-
-void Write_MS (unsigned char PortName, unsigned char Data);
