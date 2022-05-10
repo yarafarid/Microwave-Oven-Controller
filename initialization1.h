73ee1501fc0ce1fd56bad75aa86a75d4bc276d40
FarahMohamed27 <105306483+FarahMohamed27@users.noreply.github.com>
2022-05-10 22:16:57 +0200

    init1.h

---
+++ initialization1.h
@@ -0,0 +1,9 @@
+void GPIO_PinDirection (unsigned char PortName, unsigned char PinNumber, unsigned char Direction);
+	
+void GPIO_PinToggle (unsigned char PortName, unsigned char PinNumber);
+
+void Port_Write (unsigned char PortName, unsigned char Data);
+
+void Port_Write (unsigned char PortName, unsigned char Data);
+
+void Write_MS (unsigned char PortName, unsigned char Data);
