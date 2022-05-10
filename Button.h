1b5f79bc184454e3a1c374f0f38365bbe6566fc1
yarafarid <87917403+yarafarid@users.noreply.github.com>
2022-05-10 18:17:58 +0200

    Add files via upload
    Adding button header file

---
+++ Button.h
@@ -0,0 +1,4 @@
+void Button_vInit(unsigned char portName , unsigned char pinNo);
+unsigned char Button_u8read(unsigned char portName , unsigned char pinNo);
+void Button_vInit_PUll_UP(unsigned char portName , unsigned char pinNo);
+
