8901e9793b3a0263bf2666a7ec2f0ed94cd3cc41
yarafarid <87917403+yarafarid@users.noreply.github.com>
2022-05-11 17:07:44 +0200

    Button header file

---
+++ Button.h
@@ -0,0 +1,3 @@
+void Button_Init(unsigned char portName , unsigned char pinNo);
+unsigned char Button_Read(unsigned char portName , unsigned char pinNo);
+void Button_Init_PUll_UP(unsigned char portName , unsigned char pinNo);
