d5a892b8b027150f6f2ea0e01440c4ad2452bb14
ReemAbdelbary <101728693+ReemAbdelbary@users.noreply.github.com>
2022-05-12 18:50:06 +0200

    LCDfinalDriver.h

---
+++ LCDfinalDriver.h
@@ -0,0 +1,8 @@
+void LCD_init(void);
+void LCD_Cmd(unsigned char command);
+void LCD_PrintString(char *String);
+void readyToPrint(void);// must call it before print 
+static void CREATEpulse (void);// give pulse after any print command
+void sendChar(unsigned char ch);	
+void ClearLcd(void);
+void ForceCursor(unsigned char row,unsigned char col);//rows are 1,2 col 1 to 18
