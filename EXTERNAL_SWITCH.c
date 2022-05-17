28956dc374203a4c40ee8da2edecb2263367d681
yarafarid <87917403+yarafarid@users.noreply.github.com>
2022-05-17 21:28:58 +0200

    just changing the pin number

--- EXTERNAL_SWITCH.c
+++ EXTERNAL_SWITCH.c
@@ -4,22 +4,21 @@
 #include "Initialization1.h"
 #include "Button.h"
 #include "LCDfinalDriver.h"
-#include <TM4C123.h>
-#include "Io.h"
+
 
 void Door_Init() {
-Button_Init('A' , 2);
-GPIO_PinDirection ('A', 2 ,0) ;
+Button_Init('D' , 3);
+GPIO_PinDirection ('D', 3 ,0) ;
 }
 void Check_The_Door(){
-	while (!Button_Read('A', 2)){ // Switch is pressd--> open door
+	while (!Button_Read('D', 3)){ // Switch is pressd--> open door
 		LCD_PrintString("Please Close The Door") ;  //Not Ready to cook 
 	} 
 	LCD_PrintString("Enter a choice A, B, C or D") ;
 }
 unsigned char Door_Status()
 {
-	if (!Button_Read('A', 2)) {
+	if (!Button_Read('D', 3)) {
 		return 1 ;
 	}
 	else  {return 0;}
