9b78993be9021b230833de728d54d91503d3ede1
FarahMohamed27 <105306483+FarahMohamed27@users.noreply.github.com>
2022-05-23 15:03:16 +0200

    BandC_func.c

---
+++ BandC_func.c
@@ -0,0 +1,94 @@
+#include "tm4c123gh6pm.h"
+#include "stdint.h"
+#include "LCDfinalDriver.h"
+#include "stdio.h"
+#include "ourKeypad.h"
+#include "Button.h"
+#include "init2.h"
+#include "Initialization1.h"
+#include "delaySys.h"
+#include "countDown.h"
+#include "LedsBlink.h"
+#include "Buzzer.h"
+#include "EXTERNAL_SWITCH.h"
+
+
+ void BeefOrChicken(unsigned char cook)
+ { 
+	 int DoorCheck =0;
+	 int time;
+	 int min_b_c ;
+	 int sec_b_c ;
+	 int sec_b_c_1 ;
+	 int sec_b_c_2 ;
+	 int weight;
+	 int error_check = 0;
+	 if(cook=='B')
+	 {
+		 LCD_PrintString("Beef weight");
+		 delay_inMilli(1000);
+		 ClearLcd();
+	 }
+	 else if (cook=='C')
+	 {
+		 LCD_PrintString("Chicken weight");
+		 delay_inMilli(1000);
+		 ClearLcd();
+	 }
+	 
+	 LCD_PrintString("weight : ");
+	 weight = keypad_Read() ;
+	 sendChar(weight);
+	 delay_inMilli(2000);
+	 ClearLcd();
+	 if((((weight - '0')>=1) && ((weight- '0') <=9))){
+					if(cook=='B')
+					 {
+						 time = (weight - '0') * 30;	 
+						 LCD_PrintString("Time : ");
+						 delay_inMilli(1000);
+						 min_b_c = time / 60;
+						 sec_b_c = time%60;
+						 sec_b_c_1 = sec_b_c /10;
+						 sec_b_c_2 = sec_b_c %10;
+						 sendChar(min_b_c + '0');
+						 sendChar(':');
+						 sendChar(sec_b_c_1+ '0');
+						 sendChar(sec_b_c_2+ '0');	
+						 delay_inMilli(1000);				
+						 ClearLcd();
+					 }
+					else if (cook=='C')
+					 {
+						 time = (weight - '0') * 12 ;
+						 LCD_PrintString("Time : ");
+						 delay_inMilli(1000);
+						 min_b_c = time / 60;
+						 sec_b_c = time%60;
+						 sec_b_c_1 = sec_b_c /10;
+						 sec_b_c_2 = sec_b_c %10;
+						 sendChar(min_b_c + '0');
+						 sendChar(':');
+						 sendChar(sec_b_c_1+ '0');
+						 sendChar(sec_b_c_2+ '0');
+						 delay_inMilli(1000);
+						 ClearLcd();
+					 }			 
+			 }						
+			else 
+				{
+				 LCD_PrintString("Error");
+				 delay_inMilli(2000);
+				 ClearLcd();
+				 if(cook == 'B'){BeefOrChicken('B');}
+				 else{BeefOrChicken('C');}
+				 error_check++;
+				}
+	 if(error_check == 0){
+
+	 count_down(time);
+	 LCD_PrintString("End of cooking");
+	 delay_inMilli(2000);
+	 ClearLcd();
+	 }
+ }
