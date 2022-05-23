cc5df241b32ade1b62235bf22d6f77fe3a82f4c2
yarafarid <87917403+yarafarid@users.noreply.github.com>
2022-05-23 07:41:34 +0200

    Appearing of minutes part on LCD during count down

--- countDown.c
+++ countDown.c
@@ -12,25 +12,40 @@
 #include "init2.h"
 #include "delaySys.h"
 
-void count_down(int time)
+void count_down(int totalTime)
  {	
-	 unsigned char cook_time,pause_flag = 0;
-	 unsigned char Time_At_Pause  = 0;
-         int i  ;
-	 char t[100];
-	 for(i = time ; i>0; i--)
+	 int sec = totalTime%60;
+	 int min = totalTime/60;
+       	int totalTimePause = 0;
+	 int SW1Pressed = 0;
+	 int minAtPause = 0;
+	 int sec_At_Pause  = 0;
+	
+	 unsigned char pause_flag = 0;
+	 
+         int i = sec ;
+	 char t_m[100] , t_s[100];
+	for(;min >= 0 ; min--){
+	 for( ; i>0; i--)
 	 {
 		 LCD_PrintString("counting : ");
 						 if((Button_Read('F',4)==0 && (pause_flag == 0) )|| (Door_Status()== 1))//sw1 pressed for the first time)
 						 {
 							 ClearLcd();
-							 Time_At_Pause = i+1;
+							 sec_At_Pause = i+1;
+                                                         minAtPause = min;
+							 totalTimePause = (minAtPause*60) + (sec_At_Pause);
 							 pause_flag = 1 ;
 							 LCD_PrintString("first step");
 							 delay_inMilli(2000);
 							 ClearLcd();
-							 sprintf(t,"%d",i+1);
-							 LCD_PrintString(t);
+							 sprintf(t_m,"%d",minAtPause);
+							 LCD_PrintString(t_m);
+					
+							 sendChar(':');
+							 sprintf(t_s,"%d",i+1);
+							 LCD_PrintString(t_s);
+							 
 							 
 							 while(!(Button_Read('F',4)== 0 && (pause_flag ==1)) || (Door_Status()== 1)) 
 												
@@ -39,14 +54,15 @@ void count_down(int time)
 									if (Button_Read('F',0)== 0 ){ 
 									ClearLcd();
 									delay_inMilli(1000);
-									count_down(Time_At_Pause) ;
-													
+									count_down(totalTimePause) ;
+										break ;			
 								         }
 									else if(!(Button_Read('F',0)== 0 ) && (Door_Status()== 1) ){
 									ClearLcd();
 									delay_inMilli(1000);
-									 count_down(Time_At_Pause) ;
-									}
+									 count_down(totalTimePause) ;
+										
+								}
 							
 					                         }
 							SW1Pressed +=1;					
@@ -54,11 +70,23 @@ void count_down(int time)
 						 }
 			
 			                        LedOn();
-	                                       sprintf(t,"%d",i);
-			                      LCD_PrintString(t);
+	                                       sprintf(t_m,"%d",min);
+						LCD_PrintString(t_m);
+						 sendChar(':');
+						 sprintf(t_s,"%d",i);
+						 LCD_PrintString(t_s);
+			
 		                              delay_inMilli(1000);
 		                              ClearLcd();
  }
+		if(i == 0){
+		sec = 59 ;
+		i = sec ;	
+		}
+	else if (SW1Pressed == 1){
+	 break;
+	 }
+	}
 	 
 	 
  
@@ -66,17 +94,8 @@ void count_down(int time)
 			         
 				
 	    ClearLcd();
-	    LCD_PrintString("cooking stopped");
-		            //ideal state
-	  while(!(Button_Read('F',0)== 0 && (pause_flag ==1) && (Door_Status()==1)))
-		{	
-		   LedBlinking();
-		}
+	    LCD_PrintString("cooking stopped");		            
 	}
-	   if (Button_Read('F',0)== 0 && (Door_Status()==0)) {
-			 // back to main
-			 // switch_function() ;
-		 }
 	
 	LedOff();
 	delay_inMilli(1000);
